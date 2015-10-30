8f4ae10c68804fc4ed411cdfa4e0ef87f4954403
Serg09 <sergeyskumatov@aol.com>
2015-10-30 08:58:56 -0600

    Updated messages
    [#106915360]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,11 @@
 class Student < ActiveRecord::Base
   # validates :first_name, :last_name, :email, :cell_phone, presence: true
   with_options if: :first_name do |name|
-    name.validates :first_name, length: { minimum: 2 },
-      format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
+    name.validates :first_name, length: { minimum: 3 },
+      format: { with: /\A[a-zA-Z]+\z/, message: "should only contain letters" },
       presence: true
     name.validates :last_name, length: { minimum: 2 },
-      format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, presence: true
+      format: { with: /\A[a-zA-Z]+\z/, message: "should only contain letters" }, presence: true
     name.validates :email,
       format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},
       presence: true
@@ -24,13 +24,13 @@ class Student < ActiveRecord::Base
   end
     validates :cell_phone,
       format: { with: /\A[^a-zA-Z]+\z/,
-      message: "only allows numbers" }
+      message: "should only contain numbers" }
     validates :home_phone,
       format: { with: /\A[^a-zA-Z]+\z/,
-      message: "only allows numbers" }
+      message: "should only contain numbers" }
     validates :work_phone,
       format: { with: /\A[^a-zA-Z]+\z/,
-      message: "only allows numbers" }
+      message: "should only contain numbers" }
     validates_each :first_name, :last_name do |record, attr, value|
       record.errors.add(attr, "must start with Upper case") if
       value =~ /\A[[:lower:]]/
